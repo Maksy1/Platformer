10af3843d5dd0815d09ac542e1954e4073ab3928
Maksy1 <161631086+Maksy1@users.noreply.github.com>
2024-03-18 19:41:16 +0000

    Translated to Godot 4

--- scripts/start scene.gd
+++ scripts/start scene.gd
@@ -4,8 +4,8 @@ extends Control
 
 
 func _on_Play_button_down():
-	get_tree().change_scene("res://menus/Levels.tscn")
+	get_tree().change_scene_to_file("res://menus/Levels.tscn")
 
 
 func _on_Controls_button_down():
-	get_tree().change_scene("res://menus/Settings.tscn")
+	get_tree().change_scene_to_file("res://menus/Settings.tscn")
