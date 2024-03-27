ab1ebce33ffc6e4ef565dad97ce621a4eafceaac
Maksy1 <161631086+Maksy1@users.noreply.github.com>
2024-03-27 15:53:29 +0000

    level1

---
+++ Player.gd
@@ -0,0 +1,6 @@
+extends CharacterBody2D
+
+#creating a variable
+var dir=Vector2(0,0)
+
+
--- scripts/start scene.gd
+++ scripts/start scene.gd
@@ -9,3 +9,5 @@ func _on_Play_button_down():
 
 func _on_Controls_button_down():
 	get_tree().change_scene_to_file("res://menus/Settings.tscn")
+
+	
